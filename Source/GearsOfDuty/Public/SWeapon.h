c63e1a1bf42c9d0466d425dedbc6187f72b6d96a
Danny Tharma <dltcorreo@gmail.com>
2019-02-12 15:36:13 -0500

    Perfected the gun holding omg

---
+++ Source/GearsOfDuty/Public/SWeapon.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SWeapon.generated.h"
+
+// forward declaring for optimization
+class USkeletalMeshComponent;
+
+UCLASS()
+class GEARSOFDUTY_API ASWeapon : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ASWeapon();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	USkeletalMeshComponent* MeshComp;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
